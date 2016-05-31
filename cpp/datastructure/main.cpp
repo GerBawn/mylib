f69fde044783b4c697f6e39148a2b2090cf01aa4
GerBawn <lingchen410@163.com>
2016-05-31 18:32:01 +0800

    完成队列的实现

--- cpp/datastructure/main.cpp
+++ cpp/datastructure/main.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
-#include "List/Stack.h"
+#include "List/ArrayQueue.h"
+#include "List/ListQueue.h"
 
 using namespace std;
 
 int main() {
-    Stack<int> al;
-    al.push(5);
-    al.push(2);
-    al.push(10);
-    al.print();
+    ListQueue<int> aq;
+    aq.enqueue(6);
+    aq.enqueue(3);
+    aq.enqueue(12);
+    aq.enqueue(9);
+    aq.enqueue(2);
+    aq.print();
     int a;
-    al.pop(a);
-    al.print();
-    cout<<al.size()<<endl;
-    al.flush();
-    cout<<al.size()<<endl;
-    al.print();
-//    al.insert(9);
-//    al.print();
-//    al.insert(20);
-//    al.print();
-//    al.insert(30);
-//    al.print();
-//    cout<<al.size()<<endl;
-//    cout<<al.getValue(0)<<endl;
-//    al.setPos(1);
-//    cout<<al.getValue()<<endl;
-//    al.prev();
-//    cout<<al.getValue()<<endl;
-//    al.next();
-//    al.next();
-//    cout<<al.getValue()<<endl;
+    aq.dequeue(a);
+    aq.print();
+    aq.dequeue(a);
+    aq.print();
+    aq.enqueue(20);
+    aq.print();
     return 0;
 }
